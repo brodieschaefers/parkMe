e55aaf2f033a9787ed187bfaf3e3aedd67cca6b7
nolimitcarter <carterthayer@protonmail.com>
2021-05-06 20:34:53 -0400

    upload

---
+++ parkMe/parkMe/parkMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  parkMeApp.swift
+//  parkMe
+//
+//  Created by root on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct parkMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
